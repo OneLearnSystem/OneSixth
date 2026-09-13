02c2a6835992af243e421c20e62160fcac19c481
Crunchy <masonsandersbussiness@gmail.com>
2026-09-13 16:16:29 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 // Set the SAME new project values in all five apps. Never put a secret/service-role key here.
 window.ONELEARN_CONFIG = {
   "product": "OneSixth",
-  "supabaseUrl": "",
-  "supabaseKey": "",
+  "supabaseUrl": "https://nndjwqjyfgozltdoujiu.supabase.co",
+  "supabaseKey": "sb_publishable_fWD8fxrR9-u1oslITaCZhw_WAooeP69",
   "apps": {
     "OneLearn": "../OneLearn/",
     "OneEducation": "../OneEducation/",
